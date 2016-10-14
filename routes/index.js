e4b44b88e61fb18d6148f6bb2a3fb30d7b9b55fd
Sanjna Verma <sanjnav17@gmail.com>
2016-10-14 02:12:07 -0400

    adding games -- can get api top 100 songs

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var billboard = require("billboard-top-100");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/design', function(req, res, next) {
-  res.render('design', { title: 'Express' });
+router.get('/game', function(req, res, next) {
+  res.render('game', { title: 'Express' });
+  billboard(function(songs){
+    console.log(songs); //prints array of top 100 songs
+  });
 });
 
 router.get('/writing', function(req, res, next) {
