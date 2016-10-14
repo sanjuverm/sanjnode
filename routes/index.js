38512db2dfe4a02f2f30f0e7b5a324355c28b5d9
Sanjna Verma <sanjnav17@gmail.com>
2016-10-14 11:47:19 -0400

    trying to add lyrics but its not working rn

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var billboard = require("billboard-top-100");
+var lyrFetchAPI = require('lyrics-fetcher');
+var lyrics = require('gimme-lyrics');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,14 +12,24 @@ router.get('/', function(req, res, next) {
 router.get('/game', function(req, res, next) {
   //Pick a number at random from 0 to 100
   var randomNum = Math.floor((Math.random() * 100) + 1);
-
+  var magicSongTitle;
+  var magicSongArtist;
   // if(!err){
     billboard(function(songs){
       //console.log(songs); //prints array of top 100 songs
       // console.log(songs[randomNum]);
-      var magicSong = songs[randomNum];
-      res.render('game', { title: 'Express' , billboard100songs: songs, magicSong:magicSong});
+      var magicSongObj = songs[randomNum];
+      // magicSongTitle = magicSongObj.title;
+      // magicSongArtist = magicSongObj.artist;
+      magicSongTitle = "paradise";
+      magicSongArtist = "coldplay"
+
+      // res.render('game', { title: 'Express' , title:magicSongTitle, artist: magicSongArtist});
     });
+    lyrics.getLyrics('Drake', 'Hotline Bling', function(err, lyrics) {
+      console.log(err || lyrics);
+    });
+    res.render('game', { title: 'Express' , title:magicSongTitle, artist: magicSongArtist});
   // }
 });
 
