8e6e891d230848e00caecd9c7abb2d6ab6806097
sanjuverm <sanj@nyu.edu>
2017-01-11 22:36:20 -0800

    Trying to fix package.json

--- routes/index.js
+++ routes/index.js
@@ -11,25 +11,25 @@ router.get('/', function(req, res, next) {
 
 router.get('/game', function(req, res, next) {
   //Pick a number at random from 0 to 100
-  var randomNum = Math.floor((Math.random() * 100) + 1);
-  var magicSongTitle;
-  var magicSongArtist;
-  // if(!err){
-    billboard(function(songs){
-      //console.log(songs); //prints array of top 100 songs
-      // console.log(songs[randomNum]);
-      var magicSongObj = songs[randomNum];
-      // magicSongTitle = magicSongObj.title;
-      // magicSongArtist = magicSongObj.artist;
-      magicSongTitle = "paradise";
-      magicSongArtist = "coldplay"
-
-      // res.render('game', { title: 'Express' , title:magicSongTitle, artist: magicSongArtist});
-    });
-    lyrics.getLyrics('Drake', 'Hotline Bling', function(err, lyrics) {
-      console.log(err || lyrics);
-    });
-    res.render('game', { title: 'Express' , title:magicSongTitle, artist: magicSongArtist});
+  // var randomNum = Math.floor((Math.random() * 100) + 1);
+  // var magicSongTitle;
+  // var magicSongArtist;
+  // // if(!err){
+  //   billboard(function(songs){
+  //     //console.log(songs); //prints array of top 100 songs
+  //     // console.log(songs[randomNum]);
+  //     var magicSongObj = songs[randomNum];
+  //     // magicSongTitle = magicSongObj.title;
+  //     // magicSongArtist = magicSongObj.artist;
+  //     magicSongTitle = "paradise";
+  //     magicSongArtist = "coldplay"
+  //
+  //     // res.render('game', { title: 'Express' , title:magicSongTitle, artist: magicSongArtist});
+  //   });
+  //   lyrics.getLyrics('Drake', 'Hotline Bling', function(err, lyrics) {
+  //     console.log(err || lyrics);
+  //   });
+    res.render('game', { title: 'Express' });//, title:magicSongTitle, artist: magicSongArtist});
   // }
 });
 
